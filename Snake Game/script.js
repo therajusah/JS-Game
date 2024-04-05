07f097586f3398f58b5e4db66a637c445b900f0c
therajusah <workwithrajukumar@gmail.com>
2024-04-05 11:54:01 +0530

    "Initialize snake game with basic functionality; update script.js and style.css"
    The changes include:
    - Adding a DOMContentLoaded event listener to start the game when the document is loaded
    - Defining game variables such as the game arena, arena size, cell size, score, gameStarted, food, and snake positions
    - Defining a startGame function that initializes the game
    - Updating the style.css file to adjust the width of the game-arena to match the arenaSize variable

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,1 +1,22 @@
-console.log("HI");
+document.addEventListener("DOMContentLoaded", () => {
+    const gameArena = document.getElementById("game-arena");
+const arenaSize = 600;
+const cellSize = 20;
+let score = 0;
+let gameStarted = false;
+let food = {x: 300, y: 200};
+let snake = [{x:160, y:200}, {x:140, y: 200}, {x: 120, y:200}];
+let Dx = cellSize; // displacement on x axis
+let Dy = 0; // displacement on y axis
+
+
+
+function startGame() {
+
+
+
+}
+
+startGame();
+
+});
